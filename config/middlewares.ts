df4a879cfff44c03e5831de77aa31e39a07dcceb
Saif Shahriar <sh.saif@proton.me>
2026-08-30 17:44:06 +0600

    feat: cors for frontend origin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,24 @@
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	'strapi::logger',
+	'strapi::errors',
+	'strapi::security',
+	{
+		name: 'strapi::cors',
+		config: {
+			origin: [
+				'http://localhost:3000',
+				...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : []),
+			],
+		},
+	},
+	'strapi::poweredBy',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
 ];
 
 export default config;
